66d5181721d424eeaebd0d32de4b9fb829bcf363
Seva <seva213415@inbox.ru>
2017-08-28 15:50:19 +0300

    version 6.0

--- source/format.js
+++ source/format.js
@@ -6,21 +6,21 @@ const format = (arr, column) => {
     const arrLengthColumn = new Array(column);		  
     arrLengthColumn.fill(0);
 
-    arrStringFormat.forEach( (item, i) => {
+    arrStringFormat.forEach((item, i) => {
         const index = i % column; 									// номер столбца
         if ( item.length > arrLengthColumn[index] ) {   			// наибольшая длина элемента в столбце
             arrLengthColumn[index] = item.length;
         }
     });
 
-    return arrStringFormat.reduce(  (resultFormat, item, i) => {
+    return arrStringFormat.reduce((resultFormat, item, i) => {
         const index = i % column; 									// номер столбца
         let numberOfSpaces = arrLengthColumn[index] - item.length;  // выравнивание
         if (index != 0) {											// не первая колонка
         	numberOfSpaces++;							
         }
         const spaces = new Array(numberOfSpaces + 1).join(' ');
-        resultFormat += spaces + item + ( (index == column - 1 && i != arrStringFormat.length - 1) ? '\n' : '' );
+        resultFormat += spaces + item + ((index == column - 1 && i != arrStringFormat.length - 1) ? '\n' : '' );
 
         return resultFormat;        
     }, '');
