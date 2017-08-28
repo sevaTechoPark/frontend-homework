7b297190a9031f412f0cfed8150b609aa5483681
Seva <seva213415@inbox.ru>
2017-08-28 11:03:27 +0300

    version 4.0

--- source/format.js
+++ source/format.js
@@ -2,39 +2,29 @@
 
 const format = (arr, column) => {
 
-    let arrStringFormat = arr.join(',').split(','); // массив чисел => массив строк
+    const arrStringFormat = arr.map( (item) => {	// массив чисел => массив строк
+    	return item.toString();
+    });
 
-    let arrLengthColumn = new Array(column);
+    const arrLengthColumn = new Array(column);		  
     arrLengthColumn.fill(0);
 
     arrStringFormat.forEach( (item, i) => {
-
-        const index = i % column; // номер столбца
-
+        const index = i % column; 						// номер столбца
         if ( item.length > arrLengthColumn[index] ) {   // наибольшая длина элемента в столбце
-
             arrLengthColumn[index] = item.length;
         }
-
     });
 
-    const size = Math.ceil( arr.length / column );
-    let resultFormat = arrStringFormat.reduce(  (result,item, i) => {
-
-        const index = i % column; // номер столбца
-
+    return arrStringFormat.reduce(  (resultFormat, item, i) => {
+        const index = i % column; 									// номер столбца
         let numberOfSpaces = arrLengthColumn[index] - item.length;  // выравнивание
-
-        if (index != 0) numberOfSpaces++;	// не первая колонка
-
+        if (index != 0) {											// не первая колонка
+        	numberOfSpaces++;							
+        }
         const spaces = new Array(numberOfSpaces + 1).join(' ');
+        resultFormat += spaces + item + ( (index == column - 1 && i != arrStringFormat.length - 1) ? '\n' : '' );
 
-        result += spaces + item + ( (index == column - 1 && i != arrStringFormat.length - 1) ? '\n' : '' );
-
-        return result;
-        
+        return resultFormat;        
     }, '');
-
-    return resultFormat;
-
 };
